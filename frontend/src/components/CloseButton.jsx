cee08fe445018db6dfe5fcfd013451c259789db4
viktoriia <viktoriia.safonova@hyperisland.se>
2024-08-19 23:53:01 +0200

    Change button to div because although it doesn't work on iOS

--- frontend/src/components/CloseButton.jsx
+++ frontend/src/components/CloseButton.jsx
@@ -1,16 +1,14 @@
-import { Button } from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import PropTypes from "prop-types";
 
 export default function CloseButton({ onClick }) {
   return (
-    <Button
+    <div
       className="absolute bottom-48 left-4 p-2 bg-white text-black rounded-full"
       onClick={onClick}
     >
-      Fuck apple
       <XMarkIcon className="h-5 w-5" />
-    </Button>
+    </div>
   );
 }
 
