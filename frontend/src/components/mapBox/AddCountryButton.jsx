e1016a56288ad5591478a90e4468ed1d1f0dc18d
viktoriia <viktoriia.safonova@hyperisland.se>
2024-09-11 02:19:56 +0200

    Change the size of add button

--- frontend/src/components/mapBox/AddCountryButton.jsx
+++ frontend/src/components/mapBox/AddCountryButton.jsx
@@ -5,7 +5,7 @@ export default function AddCountryButton({ onClick }) {
   return (
     <button
       onClick={onClick}
-      className="absolute bottom-0 bg-indigo-500 left-2 mb-2 rounded-full w-10 h-10 flex justify-center items-center"
+      className="absolute bottom-0 bg-indigo-500 left-2 mb-2 rounded-full w-14 h-14 flex justify-center items-center"
     >
       <PlusIcon className="h-7 w-7 text-white" />
     </button>
